bd75c02fd0573d0998377963a296a76643bf8b84
Nalugsw <00001128535750sp@al.educacao.sp.gov.br>
2026-03-06 10:19:49 -0300

    Textbox

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -87,7 +87,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(180, 153);
+            this.label1.Location = new System.Drawing.Point(187, 151);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(272, 24);
             this.label1.TabIndex = 4;
@@ -97,18 +97,18 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(487, 153);
+            this.textBox1.Location = new System.Drawing.Point(461, 153);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(40, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.UseWaitCursor = true;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(487, 200);
+            this.textBox2.Location = new System.Drawing.Point(461, 200);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(40, 20);
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
             this.textBox2.UseWaitCursor = true;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
@@ -118,7 +118,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(180, 196);
+            this.label2.Location = new System.Drawing.Point(187, 194);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(272, 24);
             this.label2.TabIndex = 6;
